af6e6eac4d131d587ae7e244e644804f7a6ed608
Oluwagbenga Bello <bellogbenga43@gmail.com>
2025-03-20 17:44:54 +0100

    updated the link

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,8 @@ app.use(
     origin: [
       "https://loka-1.onrender.com",
       // "http://localhost:5173",
-      "https://lokatest.vercel.app",
+      // "https://lokatest.vercel.app",
+      "https://loka-frontend.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
